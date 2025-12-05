60fc9607c4b4100715519aff40cb987bf8aa9a16
Molik Agarwal <a.molik@iitg.ac.in>
2025-12-05 09:37:35 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             System.out.println(input + ": command not found");
-            if(input.equals("exit")){ {
+            if(input.equals("exit")){ 
                 break;
             }
         }
