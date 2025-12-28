f373d483b66de774f9fb53ddb2113fef34380d82
Molik Agarwal <a.molik@iitg.ac.in>
2025-12-28 15:18:28 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        String cwd[] = System.getenv("PATH").split(":");
         while(true){
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
@@ -23,15 +27,27 @@ public class Main {
                     System.out.println("type: missing operand");
                     continue;
                 }
-                if((!in[1].equals("echo")) && (!in[1].equals("type")) && (!in[1].equals("exit"))){
-                    System.out.println(in[1] + ": not found");
+                if((in[1].equals("echo")) || (in[1].equals("type")) || (in[1].equals("exit"))){
+                    System.out.println(in[1] + " is a shell builtin");
                 }
                 else {
-                    System.out.println(in[1] + " is a shell builtin");
+                    String cmd = in[1];
+                    boolean ok = false;
+                    for(String dir : cwd){
+                        Path p = Path.of(dir , cmd);
+                        if(Files.exists(p) && Files.isExecutable(p)){
+                            System.out.println(cmd + " is " + p.toString());
+                            ok = true;
+                            break;
+                        }
+                    }
+                    if(!ok){
+                        System.out.println(input + ": not found");
+                    }
                 }
             }
             else{
-                System.out.println(input + ": command not found");
+                System.out.println(input + ": not found");
             }
         }
     }
