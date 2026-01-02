ab60547c10bebe208f8e73184e89348aeb5c96bd
Molik Agarwal <a.molik@iitg.ac.in>
2026-01-02 21:58:33 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -97,6 +97,7 @@ public class Main {
                         if(!redir.isEmpty()){
                             Path filePath = Path.of(curDir.toString(),redir).normalize();
                             Files.createDirectories(filePath.getParent());
+                            pb.redirectErrorStream(true);
                             pb.redirectOutput(filePath.toFile());
                             Process process = pb.start();
                             int exitcode = process.waitFor();
