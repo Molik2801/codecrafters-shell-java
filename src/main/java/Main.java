ad3141499ab960dcd57286d4a82a5f38b20993a5
Molik Agarwal <a.molik@iitg.ac.in>
2026-01-02 19:44:13 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -21,90 +20,88 @@ public class Main {
             String[] in = result.tokens.toArray(new String[0]);
             String redir = result.redirection;
 
-            
-            // System.out.println(Arrays.toString(input.split("")));
-            if(in[0].equals("exit")){ 
-                break;
+            if(!redir.isEmpty()){
+                ProcessBuilder pb = new ProcessBuilder(in);
+                pb.redirectOutput(Path.of(redir).toFile());
+                Process pro = pb.start();
             }
-            else if(in[0].equals("echo")){
-                String res = "";
-                for(int i = 1 ; i < in.length; i++){
-                    res += in[i];
-                    res += " ";
+            else{
+                if(in[0].equals("exit")){ 
+                    break;
                 }
-                if(!redir.isEmpty()){
-                    Path file = Path.of(redir);
-                    Files.createDirectories(file.getParent());
-                    Files.writeString(file , res , StandardOpenOption.CREATE , StandardOpenOption.TRUNCATE_EXISTING);
-                }
-                else{
+                else if(in[0].equals("echo")){
+                    String res = "";
+                    for(int i = 1 ; i < in.length; i++){
+                        res += in[i];
+                        res += " ";
+                    }
                     System.out.println(res);
                 }
-            }
-            else if(in[0].equals("type")){
-                if(in.length < 2){
-                    System.out.println("type: missing operand");
-                    continue;
+                else if(in[0].equals("type")){
+                    if(in.length < 2){
+                        System.out.println("type: missing operand");
+                        continue;
+                    }
+                    if((in[1].equals("echo")) || (in[1].equals("type")) || (in[1].equals("exit")) || (in[1].equals("pwd"))){
+                        System.out.println(in[1] + " is a shell builtin");
+                    }
+                    else {
+                        String cmd = in[1];
+                        boolean ok = false;
+                        for(String dir : cwd){
+                            Path p = Path.of(dir , cmd);
+                            if(Files.exists(p) && Files.isExecutable(p)){
+                                System.out.println(cmd + " is " + p.toString());
+                                ok = true;
+                                break;
+                            }
+                        }
+                        if(!ok){
+                            System.out.println(cmd + ": not found");
+                        }
+                    }
                 }
-                if((in[1].equals("echo")) || (in[1].equals("type")) || (in[1].equals("exit")) || (in[1].equals("pwd"))){
-                    System.out.println(in[1] + " is a shell builtin");
+                else if(in[0].equals("pwd")){
+                    System.out.println(curDir);
                 }
-                else {
-                    String cmd = in[1];
+                else if(in[0].equals("cd")){
+                    Path nex = curDir.resolve(Path.of(in[1])).normalize();
+                    if(in[1].equals("~")){
+                        nex = curDir.resolve(Path.of(System.getenv("HOME")));
+                    }
+                    if(!Files.isDirectory(nex)){
+                        System.out.println("cd: " + nex + ": No such file or directory");
+                    }
+                    else{
+                        curDir = nex;
+                    }
+                }
+                else{
+                    String cmd = in[0];
                     boolean ok = false;
+                    List<String> arg = new ArrayList<>();
                     for(String dir : cwd){
                         Path p = Path.of(dir , cmd);
                         if(Files.exists(p) && Files.isExecutable(p)){
-                            System.out.println(cmd + " is " + p.toString());
+                            arg.add(cmd);
+                            for(int i = 1 ; i < in.length ; i++){
+                                arg.add(in[i]);
+                            }
+                            Process process = new ProcessBuilder(arg).start();
+                            try(BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))){
+                                String line;
+                                while((line = reader.readLine()) != null){
+                                    System.out.println(line);
+                                }
+                            }
+                            int exitcode = process.waitFor();
                             ok = true;
                             break;
                         }
                     }
                     if(!ok){
-                        System.out.println(cmd + ": not found");
+                        System.out.println(input + ": not found");
                     }
-                }
-            }
-            else if(in[0].equals("pwd")){
-                System.out.println(curDir);
-            }
-            else if(in[0].equals("cd")){
-                Path nex = curDir.resolve(Path.of(in[1])).normalize();
-                if(in[1].equals("~")){
-                    nex = curDir.resolve(Path.of(System.getenv("HOME")));
-                }
-                if(!Files.isDirectory(nex)){
-                    System.out.println("cd: " + nex + ": No such file or directory");
-                }
-                else{
-                    curDir = nex;
-                }
-            }
-            else{
-                String cmd = in[0];
-                boolean ok = false;
-                List<String> arg = new ArrayList<>();
-                for(String dir : cwd){
-                    Path p = Path.of(dir , cmd);
-                    if(Files.exists(p) && Files.isExecutable(p)){
-                        arg.add(cmd);
-                        for(int i = 1 ; i < in.length ; i++){
-                            arg.add(in[i]);
-                        }
-                        Process process = new ProcessBuilder(arg).start();
-                        try(BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))){
-                            String line;
-                            while((line = reader.readLine()) != null){
-                                System.out.println(line);
-                            }
-                        }
-                        int exitcode = process.waitFor();
-                        ok = true;
-                        break;
-                    }
-                }
-                if(!ok){
-                    System.out.println(input + ": not found");
                 }
             }
         }
